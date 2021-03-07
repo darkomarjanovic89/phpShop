4e8a38abc29a56484472b0408a1e382ecc9adb77
CojaONIX <cojaonix@gmail.com>
2021-03-07 11:06:49 +0100

    111

--- footer.php
+++ footer.php
@@ -2,15 +2,7 @@
 <footer>
     <?php
         include "nav_inter.html"
-<<<<<<< HEAD
-    ?>sdadaad
 
-    Goran
-    aaa
-=======
     ?>
-	
-	Konacno uputstvo
-    
->>>>>>> daf182632ba1a598a878ad1b2fac9358c1b4d190
+	111
 </footer>
