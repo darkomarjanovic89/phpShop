dc5bd84013141e1ec43a9c1b85388cfdd3c72b0f
darkomarjanovic89 <sportcoachdarko@gmail.com>
2021-03-07 17:25:32 +0100

    darko update

--- footer.php
+++ footer.php
@@ -1,7 +1,64 @@
 
-<footer>
-    <?php
-        include "nav_inter.html"
-    ?>sdadaad
-    
+<footer class="container-fluid">
+      <div class="row">
+          <div class="col-xl-3">
+              <ul class="list-group list-group-mine">
+                <li class="list-group-item text-white">Product</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+          </div>
+
+          <div class="col-xl-3">
+              <ul class="list-group list-group-mine">
+                <li class="list-group-item text-white">Download</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+          </div>
+
+          <div class="col-xl-3">
+              <ul class="list-group list-group-mine">
+                <li class="list-group-item text-white">Support and service</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+          </div>
+
+          <div class="col-xl-3">
+              <ul class="list-group list-group-mine">
+                <li class="list-group-item text-white">Partner</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+              </ul>
+          </div>      
+      </div>  
+      
+      <div class="row">
+        <div class="fav col-xl-12 d-flex justify-content-center pt-5">
+          <span class="line"></span>
+            <i class="fab fa-facebook fa-3x pr-4"></i>
+            <i class="fab fa-instagram fa-3x pr-4"></i>
+            <i class="fab fa-youtube fa-3x"></i>
+            <span class="line"></span>       
+        </div>
+      </div>
+
+
+      <div class="row py-4">
+          <div class="col-xl-12 d-flex justify-content-center ">
+          <i class="far fa-copyright">team of few people</i>
+          </div>
+
+      </div>
+
+      
 </footer>
